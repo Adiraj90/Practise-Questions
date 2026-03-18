56b33f6bc50d8086e485042faa66d76c50805a95
Aditya Raj <154831235+Adiraj90@users.noreply.github.com>
2026-03-18 20:13:47 +0530

    updated

--- practise/gcd.cpp
+++ practise/gcd.cpp
@@ -42,19 +42,27 @@ using namespace std;
 //     return 1;
 // }
 
+// int findGcd(int a, int b) {
+//     int n = min(a,b);
+//     int ans = 1;
+
+//     for(int i = 1; i <= sqrt(n); i++) {
+//         if(a%i == 0 && b%i == 0) {
+//             ans = max(ans, i);
+
+//             if(a%(n/i) == 0 && b%(n/i) == 0)
+//                 ans = max(ans, n/i);
+//         }
+//     }
+//     return ans;
+// }
+
 int findGcd(int a, int b) {
-    int n = min(a,b);
-    int ans = 1;
-
-    for(int i = 1; i <= sqrt(n); i++) {
-        if(a%i == 0 && b%i == 0) {
-            ans = max(ans, i);
-
-            if(a%(n/i) == 0 && b%(n/i) == 0)
-                ans = max(ans, n/i);
-        }
+    while(a > 0 && b > 0) {
+        if(a > b) a = a%b;
+        else b = b%a;
     }
-    return ans;
+    return max(a,b);
 }
 
 int main() {
