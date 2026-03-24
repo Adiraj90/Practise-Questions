ccd789f5858001cf154f72648a66021a12e4fa27
Aditya Raj <154831235+Adiraj90@users.noreply.github.com>
2026-03-24 17:53:28 +0530

    update

--- practise/hasharrar.cpp
+++ practise/hasharrar.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 void findSol(int n, int arr[]) {
@@ -13,14 +14,32 @@ void findSol(int n, int arr[]) {
     }
 }
 
-int main() {
-   int n;
-   cout << "Enter size of array you want: ";
-   cin >> n;
-   int arr[n];
-   for(int i = 0; i < n; i++) {
-    cin >> arr[i];
-   } 
+void findSolEng(string s) {
+    vector<int> hashh(26, 0);
+    for(int i = 0; i < s.length(); i++) {
+        int idx = s[i] - 97;
+        hashh[idx] += 1;
+    }
 
-   findSol(n, arr);
+    for(int i = 0; i < hashh.size(); i++) {
+        if(hashh[i] != 0) cout << char(i + 'a') << " -> " << hashh[i] << "\n";
+    }
+}
+
+int main() {
+
+    string s;
+    cout << "enter your string: ";
+    getline(cin, s);
+
+    findSolEng(s);
+//    int n;
+//    cout << "Enter size of array you want: ";
+//    cin >> n;
+//    int arr[n];
+//    for(int i = 0; i < n; i++) {
+//     cin >> arr[i];
+//    } 
+
+//    findSol(n, arr);
 }
